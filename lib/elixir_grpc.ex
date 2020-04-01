a8e544fa6bdf1e8863489be047584eea3cd8ac98
ryanyogan <ryanyogan@gmail.com>
2020-04-01 11:50:23 -0700

    initial commit

---
+++ lib/elixir_grpc.ex
@@ -0,0 +1,18 @@
+defmodule ElixirGrpc do
+  @moduledoc """
+  Documentation for `ElixirGrpc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirGrpc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
